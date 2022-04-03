decc98762ee678cf1ffb722bce6d592f24844e25
Michadel <100150894+Rinkiiiiiii@users.noreply.github.com>
2022-04-03 16:06:15 -0400

    changing variable pD to y

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -35,14 +35,14 @@ function diagonalDifference(arr) {
     //sD = arr[0][2] + arr[1][1] + arr[2][0];
     //return Math.abs(pD-sD);
     const size = arr[0].length;
-    let pD = 0;
+    let y = 0;
     let sD = 0;
     
     for (let i = 0 , o = (size - 1); i < size , o > -1 ; i++ , o--){
-         pD += arr[i][i];
+         y += arr[i][i];
          sD += arr[i][o];
     }
-    return Math.abs(pD-sD); 
+    return Math.abs(y-sD); 
 }
 
 function main() {

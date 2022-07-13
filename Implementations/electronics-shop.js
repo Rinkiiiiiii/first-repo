343c569f67e79e063524a56de97da129bf924b71
Michadel Laptop <michadelsantos64@gmail.com>
2022-07-13 13:08:15 -0400

    electronics-shop

---
+++ Implementations/electronics-shop.js
@@ -0,0 +1,27 @@
+
+var keyboards = [4];
+var drives = [5];
+var b = 5;
+// debugging
+
+var possibilities = [];
+
+for (let i = 0 ; i < keyboards.length ; i++){
+    for (let j = 0 ; j < drives.length ; j++){
+        if (keyboards[i] + drives[j] <= b){
+            possibilities.push (keyboards[i]+drives[j]);
+
+        }
+    }
+}
+
+possibilities.sort((first, second) => second - first);
+if (possibilities.length != 0){
+    return possibilities[0];
+} else if (possibilities.length == 0){
+    return ("-1");
+}
+
+// console.log (possibilities)
+
+
